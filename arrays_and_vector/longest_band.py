603924e7a730909780a91cdef7613ba055f89fee
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-03 22:19:47 -0400

    add completed longest_band excercise: study n°4

--- arrays_and_vector/longest_band.py
+++ arrays_and_vector/longest_band.py
@@ -1,3 +1,4 @@
+from typing import List
 # Given an array containing N integers, find length of longest band.
 
 # A band is defined as a subsequence which can be reordered in such a manner
@@ -24,3 +25,22 @@ el array a un unordered set, tendremos la capacidad de hacer lookahead en tiempo
 O(1). Luego, solo tenemos que encontrar cada elemento que no tenga predecesor y
 buscar el siguiente elemento en el set. Esto nos da un O(N) + O(X) + O(Y)... que
 se traduce simplemente a O(N). """
+
+
+def longest_band(array: List) -> int:
+    """Return the length of the most consecutive integers."""
+    casted_array = set(array)  # array to set, O(1) search using unordered set
+    largest = 0
+    for index in range(len(array)):
+        current = array[index]
+        if current - 1 not in casted_array:  # Checks if we have a root for a band
+            counter = 1
+            while current + 1 in casted_array:  # Loops through a band if exists
+                counter += 1
+                current += 1
+            largest = max(largest, counter)
+    return largest
+
+
+array = [1, 9, 3, 0, 18, 5, 2, 4, 10, 7, 12, 6]
+print(longest_band(array))
