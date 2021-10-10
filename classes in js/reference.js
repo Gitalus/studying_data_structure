79597c35efc0983eccc3d086b4444d767934f043
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-09 21:24:27 -0300

    add reference type examples

--- classes in js/reference.js
+++ classes in js/reference.js
@@ -9,3 +9,20 @@ let object3 = { value: 10 };
 
 console.log(object1 === object2); // true por referenciación
 console.log(object1 === object3); // false por ser distinta referencia
+
+object1.value = 15;
+
+console.log(object2.value); // 15, ya que object 1 y 2 apuntan a la misma dirección de memoria
+
+/*
+tipos primitivos:
+    Number
+    String
+    Boolean
+    null
+    undefined
+El uso de tipos "primitivos" se refiere a que sus valores son la transformación de los bits
+que la memoria almacena.
+Los de tipo referencia, son tipos que se definen por su estructura y sus valores son la referencia
+de memoria.
+*/
