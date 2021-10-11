e630057e96549aad58fd0e23bc205e91d1260fc2
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-11 17:28:57 -0300

    add second option more optimal

--- 01-arrays/08-move-zeroes.js
+++ 01-arrays/08-move-zeroes.js
@@ -11,3 +11,21 @@ var moveZeroes = function (nums) {
         }
     }
 };
+
+function moveZeroesV2(nums) {
+    let nonZeroFounds = 0;
+    let sizeNums = nums.length;
+    for (let i = 0; i < sizeNums; i++) {
+        if (nums[i] !== 0) {
+            nums[nonZeroFounds++] = nums[i];
+        }
+    }
+
+    for (let i = nonZeroFounds; i < sizeNums; i++) {
+        nums[i] = 0;
+    }
+}
+
+const nums = [0, 0, 1];
+moveZeroesV2(nums);
+console.log(nums);
