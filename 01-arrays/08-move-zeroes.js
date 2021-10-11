377692f58fc77321b5839c4fc145f7c686580234
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-11 17:49:50 -0300

    add third option for move zeroes

--- 01-arrays/08-move-zeroes.js
+++ 01-arrays/08-move-zeroes.js
@@ -26,6 +26,17 @@ function moveZeroesV2(nums) {
     }
 }
 
+function moveZeroesV3(nums) {
+    for (let nonZeroFounds = 0, current = 0; current < nums.length; current++) {
+        if (nums[current] !== 0) {
+            let temp = nums[current];
+            nums[current] = nums[nonZeroFounds];
+            nums[nonZeroFounds++] = temp;
+        }
+    }
+}
+
 const nums = [0, 0, 1];
-moveZeroesV2(nums);
+console.log(nums);
+moveZeroesV3(nums);
 console.log(nums);
