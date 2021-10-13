67a2e1fe35180fcfb5e9fe1f4c52333ec25f5039
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-12 22:09:09 -0300

    change function to match leetCode

--- 01-arrays/09-maximum-water.js
+++ 01-arrays/09-maximum-water.js
@@ -1,18 +1,18 @@
 const array1 = [7, 1, 2, 3, 9];
 
-function findMaximumWater(arr) {
-    let size = arr.length;
+var maxArea = function (height) {
+    let size = height.length;
     let maxAreaWater = 0;
 
     for (let start = 0; start < size - 1; start++) {
         for (let corredor = start + 1; corredor < size; corredor++) {
-            let area = Math.min(arr[start], arr[corredor]) * (corredor - start);
+            const area = Math.min(height[start], height[corredor]) * (corredor - start);
             if (area > maxAreaWater) {
                 maxAreaWater = area;
             }
         }
     }
     return maxAreaWater;
-}
+};
 
-console.log(findMaximumWater(array1)); // 28
+console.log(maxArea(array1)); // 28
