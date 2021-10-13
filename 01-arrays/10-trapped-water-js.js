3eb707d3c05d5b21e754fa0ccbd94bf25ed38fa1
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-13 18:38:07 -0300

    initial commit for trappedwatter using js

---
+++ 01-arrays/10-trapped-water-js.js
@@ -0,0 +1,10 @@
+const arrayOfWater = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]; // 8
+// [] = 0;
+// [3] = 0;
+// [3, 4, 3] = 0;
+
+function getTrappedWater(arr) {
+    const biggest_left = [];
+    const biggest_right = [];
+
+}
