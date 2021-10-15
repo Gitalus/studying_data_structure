815b6649d2305a35a5e6b1951b0e2a9f21b1c2fa
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-14 21:46:27 -0300

    add explanation for palindromes solutions

---
+++ basics/palindromes.js
@@ -0,0 +1,4 @@
+// Hay muchas formas de determinar un palindromo:
+//  - Usando "two pointers" uno al inicio y uno al final y moverlos al centro
+//  - Desde el centro, expandir los "two pointers"
+//  - Duplicar la string y revertirla, luego compararlas desde el inicio
