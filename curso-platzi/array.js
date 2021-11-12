e5407b5074b462a18257fc7a4f2e50fdb6a61b3a
Italo Guajardo <italog.penaloza@gmail.com>
2021-11-11 23:19:06 -0300

    create unshift method

--- curso-platzi/array.js
+++ curso-platzi/array.js
@@ -38,6 +38,16 @@ class MyArray {
         delete this.data[this.length - 1];
         this.length--;
     }
+
+    unshift(item) {
+        for (let i = this.length; i > 0; i--) {
+            this.data[i] = this.data[i - 1];
+        }
+        this.data[0] = item;
+        this.length++;
+
+        return this.data;
+    }
 }
 
 const myArray = new MyArray();
@@ -46,4 +56,7 @@ const myArray = new MyArray();
 console.log(myArray.push("Italo"));
 console.log(myArray.push("Nati"));
 console.log(myArray.get(0));
+console.log(myArray);
+
+myArray.delete(0);
 console.log(myArray);
