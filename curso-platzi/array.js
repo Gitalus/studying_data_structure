0eb867ee70dd5eb9728cee8420a32a9f4a493ee3
Italo Guajardo <italog.penaloza@gmail.com>
2021-11-11 23:13:52 -0300

    create delete method using custom shift

--- curso-platzi/array.js
+++ curso-platzi/array.js
@@ -11,6 +11,7 @@ class MyArray {
     push(item) {
         this.data[this.length] = item;
         ++this.length;
+
         return this.data;
     }
 
@@ -18,7 +19,24 @@ class MyArray {
         const lastItem = this.data[this.length - 1];
         delete this.data[this.length - 1];
         this.length--;
+
         return lastItem;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftIndex(index);
+
+        return item;
+    }
+
+    shiftIndex(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+
+        delete this.data[this.length - 1];
+        this.length--;
     }
 }
 
