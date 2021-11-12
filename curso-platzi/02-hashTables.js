d91c412a17cb2c9c042402f51986103bca8bc0a4
Italo Guajardo <italog.penaloza@gmail.com>
2021-11-11 23:29:19 -0300

    add explanation for hash tables and add img for reference

---
+++ curso-platzi/02-hashTables.js
@@ -0,0 +1,5 @@
+/* En los hash tables, la 'key' pasa por una 'Hash function', esta function transforma
+la key que le pasamos en una dirección de memoria. Siempre que le pasemos la misma key,
+se generará la misma dirección de memoria, por esta razón los hash tables son rápidos
+para acceder a los valores */
+
