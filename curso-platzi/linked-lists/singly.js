1680d5698090a391fb39332b9d6aa99975c20ee4
Italo Guajardo <italog.penaloza@gmail.com>
2021-11-12 18:02:39 -0300

    create append and prepend methods

--- curso-platzi/linked-lists/singly.js
+++ curso-platzi/linked-lists/singly.js
@@ -13,9 +13,33 @@ class MySinglyLinkedList {
             next: null
         };
         this.tail = this.head;
-
         this.length = 1;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+
+        return this;
+    }
+
+    preprend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+
+        return this;
     }
 }
 
 let myLinkedList = new MySinglyLinkedList(1);
+
+for (i = 2; i <= 5; i++) {
+    myLinkedList.append(i);
+}
+
+console.log(myLinkedList);
